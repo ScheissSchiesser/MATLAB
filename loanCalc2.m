b3c50ebcd62788aa40eb257a053244e5ccc81f93
Eric <eric.schiesser@rochester.edu>
2014-10-24 11:50:12 -0400

    Added repayment period, pays interest and then by loan order

--- loanCalc2.m
+++ loanCalc2.m
@@ -1,4 +1,4 @@
-function [total, interest] = loanCalc2(loans,capfreq,today,capdate,payment1,payment2)
+function [total, interest] = loanCalc2(loans,today,capdate,payment1,payment2,payorder)
 todaynum = datenum(today);
 capdate = datenum(capdate);
 totalbal = 0;
@@ -7,7 +7,7 @@ startingbal = 0;
 workdate = todaynum;
 month = 365/12;
 loannames = fieldnames(loans);
-pay = [5 8 6 7 1 2 3 4];
+pay = payorder;
 paycount = 1;
 leftoverpay = 0;
 n = 0;
@@ -38,9 +38,9 @@ while workdate < capdate
 end
 
 for i = 1:8
-    disp(['Loan ' num2str(i) ' ' num2str(loans.(loannames{i}).workingbal)...
-        ' ' num2str(loans.(loannames{i}).workingbal-...
-        loans.(loannames{i}).bal)])
+%     disp(['Loan ' num2str(i) ' ' num2str(loans.(loannames{i}).workingbal)...
+%         ' ' num2str(loans.(loannames{i}).workingbal-...
+%         loans.(loannames{i}).bal)])
     totalbal = totalbal+loans.(loannames{i}).workingbal;
 end
 
@@ -51,14 +51,25 @@ disp(['Total Balance: ' num2str(totalbal)])
 disp(['Total Paid: ' num2str(totalpayment)])
 disp(['Net : ' num2str(totalbal+totalpayment)])
 %% Repayment Period
-
+paycount = 1;
+n = 0;
 while totalbal > 0;
     paytemp = payment2;
+    totalbal = 0;
+%     Pay All interest
     for i = 1:8
         apy = loans.(loannames{pay(i)}).apy;
-        p0 = loans.(loannames{pay(i)}).workingbal;
-        interest = dailyInterest(p0,apy,month);
-        bal = loans.(loannames{pay(i)}).workingbal + interest;
+        bal = loans.(loannames{pay(i)}).workingbal;
+        interest = dailyInterest(bal,apy,month);
+        paytemp = paytemp - interest;
+    end
+%     Use leftover money for paying down principal
+    for i = 1:8
+        name = loannames{pay(i)};
+        bal = loans.(name).workingbal;
+        if bal == 0 && pay(paycount) == pay(i);
+            paycount = paycount + 1;
+        end
         if pay(paycount) == pay(i) && bal > 0
             bal = bal - paytemp;
             if bal <= 0
@@ -68,10 +79,15 @@ while totalbal > 0;
             end
         end
         loans.(loannames{pay(i)}).workingbal = bal;
+        totalbal = totalbal+bal;
     end
     workdate = workdate + month;
     n = n+1;
 end
-
+findate = datestr(workdate-month);
+totalpayment =totalpayment + n.*payment2;
+total = totalpayment;
 disp('Repayment Period')
+disp(findate)
+disp(['Total Paid ' num2str(totalpayment)])
 
